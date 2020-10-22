de81891f4120fc5b99267a1fe56b1e4ab1fb24a6
selvasharesource <72768529+selvasharesource@users.noreply.github.com>
2020-10-22 14:50:57 -0400

    Update InterviewpanelMsQuestionandanswerServiceApplication.java

--- src/main/java/com/interviewpanel/InterviewpanelMsQuestionandanswerServiceApplication.java
+++ src/main/java/com/interviewpanel/InterviewpanelMsQuestionandanswerServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-//Microservice to give interview qa result
 public class InterviewpanelMsQuestionandanswerServiceApplication {
 
 	public static void main(String[] args) {
