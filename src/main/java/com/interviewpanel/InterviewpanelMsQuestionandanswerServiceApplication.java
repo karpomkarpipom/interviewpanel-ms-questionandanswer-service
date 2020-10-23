83a57f1a216e42826909978fb261555e4d00338f
selvasharesource <72768529+selvasharesource@users.noreply.github.com>
2020-10-23 09:57:41 -0400

    removed - at the end microservice for retrieving the IQAs from DB

--- src/main/java/com/interviewpanel/InterviewpanelMsQuestionandanswerServiceApplication.java
+++ src/main/java/com/interviewpanel/InterviewpanelMsQuestionandanswerServiceApplication.java
@@ -3,7 +3,7 @@ package com.interviewpanel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//microservice for retrieving the IQAs from DB-
+//microservice for retrieving the IQAs from DB
 @SpringBootApplication
 @EnableEurekaClient
 public class InterviewpanelMsQuestionandanswerServiceApplication {
