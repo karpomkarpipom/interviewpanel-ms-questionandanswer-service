84006e215d5279658acd9b47a8b9d1542ec9f79a
selvasharesource <72768529+selvasharesource@users.noreply.github.com>
2020-10-28 08:47:29 -0400

    Successfully Entering in to SpringBoot Application

--- src/main/java/com/interviewpanel/InterviewpanelMsQuestionandanswerServiceApplication.java
+++ src/main/java/com/interviewpanel/InterviewpanelMsQuestionandanswerServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class InterviewpanelMsQuestionandanswerServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("-------->Successfully Entering in to SpringBoot Application<---------");
 		SpringApplication.run(InterviewpanelMsQuestionandanswerServiceApplication.class, args);
 	}
 
